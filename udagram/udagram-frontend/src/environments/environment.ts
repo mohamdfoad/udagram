7aa601d5ae8eb1743ede6c77e1d0b04fbdfa1123
unknown <mohamd.foad@gmail.com>
2023-02-18 21:19:57 +0200

    first push

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: 'udagram-api',
+  apiHost: 'http://udagram-api-dev2222222222222.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
