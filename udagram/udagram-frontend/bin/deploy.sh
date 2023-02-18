2cd7dbe69a61ca451d3ce430befb0c12fb56ecb6
unknown <mohamd.foad@gmail.com>
2023-02-19 00:07:52 +0200

    first push

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive --acl public-read ./www  arn:aws:s3:::uda-frontend-bucket/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html arn:aws:s3:::uda-frontend-bucket/
+aws s3 cp --recursive  ./www  s3://uda-frontend-bucket/
+aws s3 cp --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://uda-frontend-bucket/
